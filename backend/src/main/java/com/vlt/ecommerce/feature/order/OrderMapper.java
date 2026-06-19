2e4fd2c6044a7e7662c2fb442bb884ba0858cf80
vylote <antran064969@gmail.com>
2026-06-19 23:19:02 +0700

    create, confirm, complete order

--- backend/src/main/java/com/vlt/ecommerce/feature/order/OrderMapper.java
+++ backend/src/main/java/com/vlt/ecommerce/feature/order/OrderMapper.java
@@ -26,6 +26,7 @@ public interface OrderMapper {
     @Mapping(target = "items", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "shop", ignore = true)
     Order toOrder(OrderRequest request);
 
     List<OrderResponse> toOrderResponses(List<Order> orders);
